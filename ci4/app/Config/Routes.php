ee9ca3158a7fe1cded9de71a962176ece70638d6
jbberbon <jbberbon@student.apc.edu.ph>
2023-03-01 20:39:37 +0800

    Add news

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -34,11 +34,11 @@ $routes->get('/', 'Home::index');
 use App\Controllers\News;
 use App\Controllers\Pages;
 
+$routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'view']);
 $routes->get('news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
-
 /*
  * --------------------------------------------------------------------
  * Additional Routing
--- ci4/app/Controllers/News.php
+++ ci4/app/Controllers/News.php
@@ -3,11 +3,13 @@
 namespace App\Controllers;
 
 use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class News extends BaseController
 {
     public function index()
     {
+
         $model = model(NewsModel::class);
 
         $data = [
@@ -18,6 +20,7 @@ class News extends BaseController
         return view('templates/header', $data)
             . view('news/index')
             . view('templates/footer');
+
     }
 
     public function view($slug = null)
@@ -34,6 +37,46 @@ class News extends BaseController
 
         return view('templates/header', $data)
             . view('news/view')
+            . view('templates/footer');
+    }
+
+    public function create()
+    {
+        helper('form');
+
+        // Checks whether the form is submitted.
+        if (!$this->request->is('post')) {
+            // The form is not submitted, so returns the form.
+            return view('templates/header', ['title' => 'Create a news item'])
+                . view('news/create')
+                . view('templates/footer');
+        }
+
+        $post = $this->request->getPost(['title', 'body']);
+
+        // Checks whether the submitted data passed the validation rules.
+        if (
+            !$this->validateData($post, [
+                'title' => 'required|max_length[255]|min_length[3]',
+                'body' => 'required|max_length[5000]|min_length[10]',
+            ])
+        ) {
+            // The validation fails, so returns the form.
+            return view('templates/header', ['title' => 'Create a news item'])
+                . view('news/create')
+                . view('templates/footer');
+        }
+
+        $model = model(NewsModel::class);
+
+        $model->save([
+            'title' => $post['title'],
+            'slug' => url_title($post['title'], '-', true),
+            'body' => $post['body'],
+        ]);
+
+        return view('templates/header', ['title' => 'Create a news item'])
+            . view('news/success')
             . view('templates/footer');
     }
 }
